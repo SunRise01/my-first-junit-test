40231a14db609fea93587f724b7d6f32dce57926
SunRise01 <43930497+SunRise01@users.noreply.github.com>
2021-02-01 03:40:54 +0200

    Update SelenideTest.java

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -1,23 +1,35 @@
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.Assert.*;
+
 
 import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
-import org.junit.Test;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 public class SelenideTest {
+	@BeforeAll
+	public static void something(){
+//		Configuration.browser="edge";
+		Configuration.startMaximized=true;
+		Configuration.driverManagerEnabled=true;
+	}
 	@Test
 	public void searchSystem() {
 		open("https://softjourn.com/");
 		$("span[class='icon-search passive']").click();
-		$("input[name='query']").setValue("java1").pressEnter();
+		$("input[name='query']").setValue("java").pressEnter();
+		$(Selectors.withText("Cloud-Based Java Tool")).should(Condition.exist);
 	}
 
 	@Test
 	public void shouldContainJava(){
 		open("https://softjourn.com/");
-		$(By.linkText("Java Development")).exists();
+		$(byText("Our expertise")).scrollTo().shouldBe(Condition.visible);
+		$(".technologies").find(Selectors.byText("Java Development")).shouldBe(Condition.visible);
 	}
 
 	@Test public void contactUs(){
