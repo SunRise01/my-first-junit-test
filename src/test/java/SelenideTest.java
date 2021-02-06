cffac2d285432e7d999c7015721af27c9e4619df
SunRise01 <43930497+SunRise01@users.noreply.github.com>
2021-02-06 06:17:02 +0200

    Update SelenideTest.java

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -2,46 +2,54 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 
-import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selectors;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.openqa.selenium.By;
+import java.util.stream.Stream;
 
 public class SelenideTest {
 	@BeforeAll
-	public static void something(){
+	public static void defaultSettings(){
 //		Configuration.browser="edge";
 		Configuration.startMaximized=true;
 		Configuration.driverManagerEnabled=true;
 	}
-	@Test
-	public void searchSystem() {
-		open("https://softjourn.com/");
-		$("span[class='icon-search passive']").click();
-		$("input[name='query']").setValue("java").pressEnter();
-		$(Selectors.withText("Cloud-Based Java Tool")).should(Condition.exist);
+
+	static String sjsite = "https://softjourn.com/";
+	static Stream<Arguments> argumentsForSearch(){
+		return Stream.of(
+				Arguments.of("java"),
+				Arguments.of("javascript")
+		);
+	}
+
+	@ParameterizedTest
+	@MethodSource("argumentsForSearch")
+	public void searchSystem(String value) {
+		new HomePage()
+			.openHomePage(sjsite)
+			.useSearch(value)
+			.findText(value);
 	}
 
 	@Test
 	public void shouldContainJava(){
-		open("https://softjourn.com/");
-		$(byText("Our expertise")).scrollTo().shouldBe(Condition.visible);
-		$(".technologies").find(Selectors.byText("Java Development")).shouldBe(Condition.visible);
+		new HomePage()
+			.openHomePage(sjsite)
+			.scrollTillVisible("Our expertise")
+			.findText("Java Development");
 	}
 
 	@Test public void contactUs(){
-		open("https://softjourn.com/");
-		$("a[href='/contact-us']").click();
-		$(By.linkText("United States")).should(Condition.exist);
-		$(By.linkText("Ukraine")).should(Condition.exist);
-		$(By.linkText("Poland")).should(Condition.exist);
-		$("input[id='name']").setValue("Serhii");
-		$("input[id='phone']").setValue("+380665375916");
-		$("input[id='email']").setValue("sergiygonchar00@gmail.com");
-		$("input[id='country']").setValue("Ukraine");
-		$("textarea[name='text']").setValue("I'm trying to test your site");
+		new ContactPage()
+			.openContactPage(sjsite)
+			.setUpForms("Serhii", "+380665375916", "sergiygonchar00@gmail.com", "Ukraine", "I'm trying to test your site")
+			.checkCountries("Ukraine", "United States", "Poland");
 	}
 }
